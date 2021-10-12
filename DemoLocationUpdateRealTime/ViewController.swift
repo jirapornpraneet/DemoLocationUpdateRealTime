01bbe646a22ecb00fd40bd71075d369da8d91489
iOS Dev <jiraporn.p@piesoft.net>
2021-10-12 11:19:05 +0700

    Initial Commit

---
+++ DemoLocationUpdateRealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoLocationUpdateRealTime
+//
+//  Created by iOS Dev on 12/10/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
